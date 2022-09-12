86010056989ba667e557a81290947f10efd78c0e
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-09-11 23:54:58 -0300

    base building

---
+++ web/src/App.tsx
@@ -0,0 +1,27 @@
+// Componentes / Propriedades
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <button title="Send 1"/>
+      <button title="Send 2"/>
+      <button title="Send 3"/>
+      <button title="Hello NLW"/>
+    </div>
+  )
+}
+
+export default App
